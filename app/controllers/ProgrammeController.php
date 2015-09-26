96aec81e8cda74215c4f3743f2ab9d26c88dc73a
MBM info <marouen.ben.ahmed1@gmail.com>
2015-09-26 20:46:45 +0100

    'vendor'

---
+++ app/controllers/ProgrammeController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MBM info
+ * Date: 26/09/2015
+ * Time: 20:42
+ */
+
+class ProgrammeController  extends  \BaseController{
+
+    public function __construct(){
+
+        $this->beforeFilter('@checkLogin');
+
+
+    }
+
+
+
+} 
